b872f94545f973bc143a4ef06bc3f9e4fb8161d7
Nataliia-Shevchenko <nata_yuna@ukr.net>
2024-01-17 09:45:54 +0200

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,13 +7,12 @@ import { AuthDto } from './dto';
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('signup')
-  signup(@Body() dto:AuthDto) {
-    console.log(req)
-    return this.authService.signup()
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
   }
 
   @Post('signin')
-  signin() {
-    return this.authService.signin()
+  signin(@Body() dto: AuthDto) {
+    return this.authService.signin(dto);
   }
 }
